8c160521f62d3e2753252c8f20f5a3592a85ac15
Thefsantos <francisconomade491@gmail.com>
2023-05-25 16:57:43 -0300

    adicionando middlewarede CORS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes/routes')
 const db = require('./database/db')
 
@@ -8,6 +9,24 @@ const app = express()
 app.use(express.json())
 
 db.connect()
+
+const allowedOrigins = [
+    'example.com',
+    'example2.com'
+]
+
+app.use(cors({
+    origin: function(origin, callback){
+        let allowed = true
+
+        // mobile
+        if(!origin) allowed = true
+
+        if(!allowedOrigins.includes(origin)) allowed = false
+
+        callback(null, allowed)
+    }
+}))
 
 app.use('/api', routes)
 
